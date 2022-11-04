48d7cbbd4c6e14ed2c707827b7f57ff9de85d053
te70 <42199000+te70@users.noreply.github.com>
2022-11-04 10:46:11 +0000

    Update index.js: convert csv to json

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const csvToJson = require('convert-csv-to-json');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+const fileInputName = 'hng_sheet.csv';
+const fileOutputName = 'hng.json';
+
+csvToJson.parseSubArray('*', ',').generateJsonFileFromCsv(fileInputName, fileOutputName);
+
+const fileJson = JSON.parse(fileOutputName);
+
+Object.entries(fileJson).forEach((entry) => {
+  const [key, value] = entry;
+  console.log(`${key}: ${value}`);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
